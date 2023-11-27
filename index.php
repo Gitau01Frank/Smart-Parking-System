e1f7e372352c92a02cfb099617a3581d93443740
Gitau01Frank <franciswainainag@gmail.com>
2023-11-28 01:21:26 +0300

    login_page

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'db_connection.php';
+include 'get_user.php';
+include 'home.php';
+include 'login.php';
+include 'logout.php';
+include 'on_login.php';
+include 'on_register.php';
+include 'register.php';
+include 'style.css';
